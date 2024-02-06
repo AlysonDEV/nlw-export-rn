fe543a62f22c2c13884951650a7f176d4f5ff446
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-06 19:27:02 -0300

    Header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/header"
 import { Text, View } from "react-native"
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-2xl font-heading">Hello world</Text>
+      <Header title="Faça seu pedido" cardQuantityItems={4} />
     </View>
   )
 }
