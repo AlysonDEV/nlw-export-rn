6376331606b77688edc80a57942431f530e5b4b0
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-06 05:13:32 -0300

    config nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native"
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View className="bg-gray-400 flex-1">
+      <Text className="text-red-200">Hello world</Text>
     </View>
   )
 }
