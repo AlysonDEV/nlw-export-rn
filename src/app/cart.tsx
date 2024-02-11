ca76b4c4950ecb4743b372969c8e438ddce85a0e
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-02-11 10:32:33 -0300

    add styles and button to back home screen

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -6,37 +6,60 @@ import { Header } from "@/components/header";
 import { Product } from "@/components/product";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Input } from "@/components/input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
-  const cartStore = useCartStore()
+  const cartStore = useCartStore();
 
-  const total = formatCurrency(cartStore.products.reduce((total, product)=> ( total + (product.price * product.quantity)),0))
-
+  const total = formatCurrency(
+    cartStore.products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    )
+  );
 
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
-      <ScrollView>
-      {cartStore.products.length > 0 ?  (
-        <View className="p-5 flex-1">
-          {
-            cartStore.products.map((product)=>(
-              <Product key={product.id} data={product} />
-            ))
-          }
-        </View>
-      ) : (
-        <Text className="font-body text-slate-400 text-center my-8">Seu carrinho está vazío</Text>
-      ) }
+      <KeyboardAwareScrollView>
+        <ScrollView>
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700 ">
+                {cartStore.products.map((product) => (
+                  <Product key={product.id} data={product} />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazío
+              </Text>
+            )}
 
-      <View className="flex-row gap-2 items-center mt-5 mb-4">
-        <Text className="text-white text-xl font-subtitle">Total: </Text>
-        <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total: </Text>
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, numero e complemento..." />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon><Feather name="arrow-right-circle" size={20}/></Button.Icon>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/" />
+
       </View>
-
-      <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, numero e complemento..." />
-      
-      </ScrollView>
     </View>
-  )
+  );
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,5 +1,11 @@
 import { TextInput, TextInputProps } from "react-native";
+import colors from "tailwindcss/colors";
 
 export function Input({...rest }: TextInputProps) {
-  return <TextInput {...rest} />
+  return <TextInput 
+  multiline 
+  textAlignVertical="top" 
+  placeholderTextColor={colors.slate[400]}
+  className="h-32 bg-slate-800 rounded-md py-4 px-3 font-body text-sm text-white"
+  {...rest} />
 }
